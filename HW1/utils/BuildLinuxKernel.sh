5892ec5dc92921b01ed525200328d6e377e874ea
mansetagunj <gunjmanseta@gmail.com>
2018-01-24 21:49:04 -0700

    Adding log functionality and cosmetic changes

--- HW1/utils/BuildLinuxKernel.sh
+++ HW1/utils/BuildLinuxKernel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-numOfCPU=
-path=
-buildSuccess=0
-kernelVersion=
+NUM_OF_CPU=
+LINUX_SOURCE_PATH=
+BUILD_SUCCESS=0
+KERNEL_VERSION=
+DATE_TIME_NOW=$(date +"%m_%d_%Y_%H_%M_%S")
+BUILD_LOG_FILE=~/repos/ECEN-5013/HW1/info_logs/build_log_$DATE_TIME_NOW.log
+
 
 if [ $# -gt 0 ]; then
 	if [ $# -lt 2 ] && [ $1 = "-h" ] || [ $1 = "--help" ] ; then
@@ -18,7 +21,7 @@ if [ $# -gt 0 ]; then
 	exit 0
         
 	elif [ $# -eq 2 ] && [ $1 = "--file" ]  || [ $1 =  "-f"  ]; then
-        	path=$2
+        	LINUX_SOURCE_PATH=$2
         else
 		echo "Invalid Option. Try -h or --help option for details."
 		exit 0               
@@ -50,23 +53,24 @@ entrypoint(){
 
 goToPath(){
 	
-	if [ -z $path ]; then
+	if [ -z $LINUX_SOURCE_PATH ]; then
 		echo "Enter the correct path to the Linux source code Dir."
 		read -p "-> " path
 
-		echo "Entered path: $path"
+		echo "Entered path: $LINUX_SOURCE_PATH"
 	fi
 
-	if [ -z $path ]; then
+	if [ -z $LINUX_SOURCE_PATH ]; then
 		echo "No path entered"
 		exit 1
-	elif [ -d $path ]; then
+	elif [ -d $LINUX_SOURCE_PATH ]; then
 		echo "Contents of Dir:"
-		cd $path
+		cd $LINUX_SOURCE_PATH
 		ls
 		echo ""
-	elif [ -d $path != 1]; then
-		echo "The file $path is not a directory."	
+		echo "Build Log can be found in $BUILD_LOG_FILE"
+	elif [ -d $LINUX_SOURCE_PATH != 1]; then
+		echo "The file $LINUX_SOURCE_PATH is not a directory."	
 	else
 		echo "Unknown Error"
 		exit 1
@@ -76,15 +80,15 @@ goToPath(){
 startBuild(){
 
 	#this is used to store the num of cores available
-	numOfCPU=$(nproc)
+	NUM_OF_CPU=$(nproc)
 	#multipying it with 2 as the number of threads per core can be 2
-	numOfCPU=$(($numOfCPU * 2))
+	NUM_OF_CPU=$(($NUM_OF_CPU * 2))
 	
 	#gives the kernelversion in the linux source tree
-	kernelVersion=$(make -s kernelversion)
+	KERNEL_VERSION=$(make -s kernelversion)
 
 	timeStart=$(date +"%x %r %Z")
-	echo "Starting Build of Kernel v$kernelVersion. Time: $timeStart"
+	echo "Starting Build of Kernel v$KERNEL_VERSION. Time: $timeStart" |tee -a $BUILD_LOG_FILE
 	echo "Do you want to continue? [YyNn]"
 	read -p "-> " ans
 	if [ $ans = "N" ] || [ $ans = "n" ]; then
@@ -99,19 +103,23 @@ startBuild(){
 
 	START=$(date +%s);
 
-	make -j $numOfCPU && make modules -j $numOfCPU && buildSuccess=1
+	make -j $NUM_OF_CPU &>> $BUILD_LOG_FILE && make modules -j $NUM_OF_CPU &>> $BUILD_LOG_FILE && BUILD_SUCCESS=1
 	
 	END=$(date +%s);
-	echo $((END-START)) | awk '{print "Build Completed, took "int($1/60)" min and "int($1%60)" sec."}'
+
+	if [ $BUILD_SUCCESS != 1 ];then
+		echo "Build Failed. Look for Build Log at: $BUILD_LOG_FILE"
+		echo "Build Failed." >> $BUILD_LOG_FILE	
+	fi
+	echo $((END-START)) | awk '{print "Build Completed, took "int($1/60)" min and "int($1%60)" sec."}' |tee -a $BUILD_LOG_FILE
 
 }
 
 startInstall(){
 	
 	echo ""
-	#echo "Build done? $buildSuccess"
-	#the varible buildSuccess is set to 1 only if the make commands for the kernel and modules build.
-	if [ $buildSuccess != 1 ]; then
+	#the variable BUILD_SUCCESS is set to 1 only if the make commands for the kernel and modules build.
+	if [ $BUILD_SUCCESS != 1 ]; then
 		echo "Kernel did not Build in this transaction."
 		echo "Do you still want to continue with the Install? [YyNn]"
 		read -p "-> " continue
@@ -122,7 +130,7 @@ startInstall(){
 	fi
 
 	timeStart=$(date +"%x %r %Z")
-        echo "Starting Install. Time: $timeStart"
+        echo "Starting Install. Time: $timeStart" |tee -a $BUILD_LOG_FILE
         echo "Do you want to continue? [YyNn]"
         read -p "-> " ans
 
@@ -133,24 +141,24 @@ startInstall(){
 	
 	START=$(date +%s);
 
-	sudo make modules_install -j $numOfCPU && sudo make install -j $numOfCPU
+	sudo make modules_install -j $NUM_OF_CPU &>> $BUILD_LOG_FILE && sudo make install -j $NUM_OF_CPU &>> $BUILD_LOG_FILE
 	
-	END=$(date +%si);
-        echo $((END-START)) | awk '{print "Install Completed, took "int($1/60)" min and "int($1%60)" sec."}'
+	END=$(date +%s);
+        echo $((END-START)) | awk '{print "Install Completed, took "int($1/60)" min and "int($1%60)" sec."}' |tee -a $BUILD_LOG_FILE
 }
 
 #gives the kernelversion in the linux source tree
-#kernelVersion=$(make -s kernelversion)
+#KERNEL_VERSION=$(make -s kernelversion)
 
 createInitrdFile(){
 
 	echo ""
 
-	#creating the initrd file with the version $kernelVersion
-	sudo update-initramfs -c -k $kernelVersion
+	#creating the initrd file with the version $KERNEL_VERSION
+	sudo update-initramfs -c -k $KERNEL_VERSION >> $BUILD_LOG_FILE 2>&1
 
 	#updating the grub file to include this kernel in the grub menu which is done by changing the grub.cfg file using the below command
-	sudo update-grub
+	sudo update-grub >> $BUILD_LOG_FILE 2>&1
 }
 
 #starting point of the bash script
