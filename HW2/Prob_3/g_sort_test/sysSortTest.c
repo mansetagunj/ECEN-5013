b6246af342748f12133c976c44aec6a353bd6447
mansetagunj <gunjmanseta@gmail.com>
2018-02-03 01:06:12 -0700

    Adding test cases

--- HW2/Prob_3/g_sort_test/sysSortTest.c
+++ HW2/Prob_3/g_sort_test/sysSortTest.c
@@ -22,44 +22,175 @@ int main()
     int *buffer = (int*)malloc(sizeof(int)*BUFFER_LEN);
     if(buffer == NULL)
     {
-	printf("Malloc Failed. Try Again\n");
-	return (-1);
+		printf("Malloc Failed. Try Again\n");
+		return (-1);
     }
 
     printf("Buffer presort.\n");
     for(int i = 0; i < BUFFER_LEN; i++)
     {
-	buffer[i] = rand()%50;
-	printf("[%d] ",buffer[i]);
+		buffer[i] = rand()%50;
+		printf("[%d] ",buffer[i]);
     }
     printf("\n");
     int *outBuffer = (int*)malloc(sizeof(int)*BUFFER_LEN);
     if(outBuffer == NULL)
     {
-	printf("Malloc Failed. Try Again\n");
-	free(buffer);
-	return (-1);
+		printf("Malloc Failed. Try Again\n");
+		free(buffer);
+		return (-1);
     }
     memset(outBuffer,0,10);	
 
-    
-    printf("Invoking 'g_ksort' system call\n");
+	printf("\n");
+    printf("Invoking 'g_ksort' system call. POSITIVE\n");
 
     long ret_status = g_sort(buffer,BUFFER_LEN,outBuffer);
          
     if(ret_status == 0) 
     {
-	printf("System call  executed correctly. Use dmesg for details.\n");
-    
-	printf("Buffer post sort.\n");
-	for(int i = 0; i < BUFFER_LEN; i++)
-	{
-		printf("[%d] ",outBuffer[i]);
-	}
-	printf("\n");		
+		printf("System call  executed correctly. Use dmesg for details.\n");
+		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		printf("Buffer post sort.\n");
+		for(int i = 0; i < BUFFER_LEN; i++)
+		{
+			printf("[%d] ",outBuffer[i]);
+		}
+		printf("\n");		
     }	
     else 
+    {
          printf("System call  did not execute as expected. Use dmesg to check. Error:%ld:%s\n",ret_status,strerror(ret_status));
+         
+ 		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		
+	}
+	
+	printf("\n");
+    printf("Invoking 'g_ksort' system call. NULL BUFFER\n");
+
+    ret_status = g_sort(NULL,BUFFER_LEN,outBuffer);
+         
+    if(ret_status == 0) 
+    {
+		printf("System call  executed correctly. Use dmesg for details.\n");
+		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		printf("Buffer post sort.\n");
+		for(int i = 0; i < BUFFER_LEN; i++)
+		{
+			printf("[%d] ",outBuffer[i]);
+		}
+		printf("\n");		
+    }	
+    else 
+    {
+         printf("System call  did not execute as expected. Use dmesg to check. Error:%ld:%s\n",ret_status,strerror(ret_status));
+         
+ 		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		
+	}
+	
+	printf("\n");
+	printf("Invoking 'g_ksort' system call. INVALID BUFFER SIZE\n");
+
+    ret_status = g_sort(buffer,1024,outBuffer);
+         
+    if(ret_status == 0) 
+    {
+		printf("System call  executed correctly. Use dmesg for details.\n");
+		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		printf("Buffer post sort.\n");
+		for(int i = 0; i < BUFFER_LEN; i++)
+		{
+			printf("[%d] ",outBuffer[i]);
+		}
+		printf("\n");		
+    }	
+    else 
+    {
+         printf("System call  did not execute as expected. Use dmesg to check. Error:%ld:%s\n",ret_status,strerror(ret_status));
+         
+ 		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		
+	}
+	
+	printf("\n");
+    printf("Invoking 'g_ksort' system call. INVALID SIZE\n");
+
+    ret_status = g_sort(buffer,0,outBuffer);
+         
+    if(ret_status == 0) 
+    {
+		printf("System call  executed correctly. Use dmesg for details.\n");
+		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		printf("Buffer post sort.\n");
+		for(int i = 0; i < BUFFER_LEN; i++)
+		{
+			printf("[%d] ",outBuffer[i]);
+		}
+		printf("\n");		
+    }	
+    else 
+    {
+         printf("System call  did not execute as expected. Use dmesg to check. Error:%ld:%s\n",ret_status,strerror(ret_status));
+         
+ 		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		
+	}
+	
+	printf("\n");
+    printf("Invoking 'g_ksort' system call. OUTBUFFER NULL\n");
+
+    ret_status = g_sort(buffer,BUFFER_LEN,NULL);
+         
+    if(ret_status == 0) 
+    {
+		printf("System call  executed correctly. Use dmesg for details.\n");
+		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		printf("Buffer post sort.\n");
+		for(int i = 0; i < BUFFER_LEN; i++)
+		{
+			printf("[%d] ",outBuffer[i]);
+		}
+		printf("\n");		
+    }	
+    else 
+    {
+         printf("System call  did not execute as expected. Use dmesg to check. Error:%ld:%s\n",ret_status,strerror(ret_status));
+         
+ 		printf("Last 10 entries dmesg log \n");
+		printf("---------------------------------------\n");
+		system("dmesg | tail -10");
+		printf("---------------------------------------\n");
+		
+	}
           
-     return (0);
+	return (0);
 }
