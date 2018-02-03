5b96d13aebbb15b4098e0bedb01d5af36d162164
mansetagunj <gunjmanseta@gmail.com>
2018-02-03 05:26:44 -0700

    Adding details to the module init and exit

--- HW2/Prob_5/gunj_timerModule.c
+++ HW2/Prob_5/gunj_timerModule.c
@@ -1,27 +1,34 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <moduleparam.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
 
-static unsigned long timer_triggered_count = 0;
-static char* name = "Gunj"
+static unsigned long timer_triggered_count = 10;
+static char* name = "Gunj";
 static unsigned long timer_timeoutMS = 500;
 
 module_param(name,charp,S_IRUGO | S_IWUSR);
 module_param(timer_timeoutMS,ulong, S_IRUGO | S_IWUSR);
-module_param(timer_triggered_count,ulong, S_IRUGO);
 
-int __init module_init()
+int __init gunjModule_init(void)
 {
-
+	printk(KERN_INFO "In the function %s\n",__FUNCTION__);
+	printk(KERN_INFO "Param Name=%s TimeoutMS=%lu\n",name,timer_timeoutMS);
+	printk(KERN_INFO "STATIC COUNT=%lu\n",timer_triggered_count++);
+	return 0;
 }
 
 
-int __exit module_exit()
+void __exit gunjModule_exit(void)
 {
 
-
+	printk(KERN_INFO "In the function %s\n",__FUNCTION__);
+	printk(KERN_INFO "STATIC COUNT=%lu\n",timer_triggered_count++);
 }
 
+
+module_init(gunjModule_init);
+module_exit(gunjModule_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Gunj Manseta");
