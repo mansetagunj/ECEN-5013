b063ea26c03fef6e1af3f036630223d79fd16902
mansetagunj <gunjmanseta@gmail.com>
2018-03-10 07:37:39 -0700

    added macros and functions for log msg struct

--- Project1/include/common/logger_task.h
+++ Project1/include/common/logger_task.h
@@ -9,8 +9,15 @@
 #ifndef LOGGER_TASK_H
 #define LOGGER_TASK_H
 
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
 #include <mqueue.h>
 #include "common_helper.h"
+#include "my_time.h"
+#include "error_data.h"
+
 
 #define LT_MSG_SIZE 20
 
@@ -43,11 +50,61 @@ typedef struct
 }LOGGERTASKQ_MSG_T;
 
 /**
+ * @brief Defines a Log struct with the name given and params with some default values
+ * 
+ */
+#define DEFINE_LOG_STRUCT(name,msgId,sourceId,format, ...) \
+    LOGGERTASKQ_MSG_T name = {      \
+        .msgID      = msgId,        \
+        .loglevel   = LOG_ALL,      \
+        .sourceID   = sourceId,     \
+        .timestamp  = {0}           \
+    };                              \
+    /*memset(name.timestamp,20,0);*/  \
+    snprintf(name.msgData,sizeof(name.msgData),format, ##__VA_ARGS__) 
+
+/**
+ * @brief Set the Log loglevel
+ * 
+ * @param log_msg 
+ * @param loglevel 
+ */
+static inline void set_Log_loglevel(LOGGERTASKQ_MSG_T *log_msg, LOG_LEVEL_T loglevel)   
+{
+    log_msg->loglevel = loglevel;
+}
+
+/**
+ * @brief Set the Log currentTimestamp to the currentTime as "sec.nsec"
+ * 
+ * @param log_msg 
+ */
+static inline void set_Log_currentTimestamp(LOGGERTASKQ_MSG_T *log_msg)   
+{
+    get_time_string(log_msg->timestamp,sizeof(log_msg->timestamp));
+}
+
+/**
  * @brief Get the Handle LoggerTaskQueue object
  * 
  * @return mqd_t 
  */
 mqd_t getHandle_LoggerTaskQueue();
+
+/**
+ * @brief Post message to the log using the log queue handle and giving log struct
+ * 
+ * @param queue 
+ * @param logstruct 
+ * @param log_struct_size 
+ */
+static inline void POST_MESSAGE_LOGTASK(mqd_t queue, const LOGGERTASKQ_MSG_T *logstruct, size_t log_struct_size)
+{
+    if(-1 == mq_send(queue, (const char*)logstruct, log_struct_size,20))
+    {
+        LOG_STDOUT(ERROR "MQ_SEND:%s\n",strerror(errno));
+    }
+}
 
 /**
  * @brief Entry point of the logger task thread
