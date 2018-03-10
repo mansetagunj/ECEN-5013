acbfe9f7304e5879eb0f08f261c9d3e7bebc4139
mansetagunj <gunjmanseta@gmail.com>
2018-03-10 07:36:22 -0700

    adding threadid to log

--- Project1/include/common/error_data.h
+++ Project1/include/common/error_data.h
@@ -17,13 +17,14 @@
 
 typedef enum{
 
-    ERR  = -1,
+    ERR     = -1,
     SUCCESS = 0,
 }RETURN_T;
 
 //syscall(SYS_gettid) [TID:%ld] ",getpid()
 
-#define LOG_STDOUT(format, ...) printf("[PID:%d]",getpid()); printf(format, ##__VA_ARGS__)
+//#define LOG_STDOUT(format, ...) printf("[PID:%d]",getpid()); printf(format, ##__VA_ARGS__)
+#define LOG_STDOUT(format, ...) printf("[PID:%d][TID:%ld]",getpid(),syscall(SYS_gettid)); printf(format, ##__VA_ARGS__)
 
 #define ERROR   "[ERROR] "
 #define INFO    "[INFO] "
