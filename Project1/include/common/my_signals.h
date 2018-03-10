0b4aa787b4c61cda9a43f4e02f5f20466f942c40
mansetagunj <gunjmanseta@gmail.com>
2018-03-10 03:40:23 -0700

    Adding signal functionality

---
+++ Project1/include/common/my_signals.h
@@ -0,0 +1,37 @@
+/*
+ * @File my_signals.h
+ *
+ * @Created on: 18-Feb-2018
+ * @Author: Gunj Manseta
+ */
+ 
+#ifndef MY_SIGNALS_H
+#define MY_SIGNALS_H
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdint.h>
+		
+#define REG_SIG_USR1	(1<<0)
+#define REG_SIG_USR2	(1<<1)
+#define REG_SIG_INT		(1<<2)
+#define REG_SIG_TERM	(1<<3)
+#define REG_SIG_TSTP	(1<<4)
+#define REG_SIG_ALL		(0x1F)
+
+typedef uint8_t REG_SIGNAL_FLAG_t ;
+
+/**
+ * @brief Register asignal handler for specific signal masks
+ * 
+ * @param sa 
+ * @param handler 
+ * @param signalMask 
+ * @return int 
+ */
+int register_signalHandler(struct sigaction *sa, void (*handler)(int), REG_SIGNAL_FLAG_t signalMask);
+
+
+
+#endif
