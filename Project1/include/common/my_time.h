46720681f5e63a49ece03963bd8077f517a5a791
mansetagunj <gunjmanseta@gmail.com>
2018-03-10 07:35:50 -0700

    Adding function to get time string

---
+++ Project1/include/common/my_time.h
@@ -0,0 +1,13 @@
+/*
+ * @File my_time.h
+ *
+ * @Created on: 18-Feb-2018
+ * @Author: Gunj Manseta
+ */
+ 
+#ifndef TIME_H
+#define TIME_H
+
+int get_time_string(char *timeString, int len);
+
+#endif
